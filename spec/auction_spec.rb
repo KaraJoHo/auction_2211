a61a2182f0c7d110aa241f8ad2f341ad5bc61edc
KaraJH <karas.jones@yahoo.com>
2023-01-08 11:46:12 -0700

    test: Add item method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Auction do
       expect(auction.items).to eq([])
     end
   end
+
+  describe '#add_item' do 
+    it 'adds item object to the list of items' do 
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq([item1, item2])
+    end
+  end
 end
