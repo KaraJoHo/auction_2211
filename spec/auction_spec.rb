2c717d4b7f7244c32df45e1dfb35214e914ec63f
KaraJH <karas.jones@yahoo.com>
2023-01-08 12:22:15 -0700

    test: Bidders method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -78,5 +78,22 @@ RSpec.describe Auction do
     end
   end
 
+  describe '#bidders' do 
+    it 'is a list of attendees who placed a bid' do 
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20) 
+      item1.add_bid(attendee1, 22) 
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15) 
+
+      expect(auction.bidders).to eq([attendee1, attendee2, attendee3])
+    end
+  end
+
   
 end
