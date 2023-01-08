dce7e58acc8eea890e404ec0fceeb20020767317
KaraJH <karas.jones@yahoo.com>
2023-01-08 12:31:30 -0700

    test: Bidder info method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -95,5 +95,27 @@ RSpec.describe Auction do
     end
   end
 
+  describe '#bidder_info' do 
+    it 'a hash of bidders with values of ahash of their bidget and array of items they bid on' do 
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20) 
+      item1.add_bid(attendee1, 22) 
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15) 
+
+      expected = {attendee1 => {budget => 50, items => [item1]},
+                  attendee2 => {budget => 75, items => [item1, item3]},
+                  attendee3 => {budget => 100, items => [item4]}
+      }
+
+      expect(bidder_info).to eq(expected)
+    end
+  end
+
   
 end
