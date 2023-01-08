f6d30db3b2b12de84dd398c4e2f518a3198d1931
KaraJH <karas.jones@yahoo.com>
2023-01-08 13:07:08 -0700

    feat: Close bidding method with passing test

--- lib/item.rb
+++ lib/item.rb
@@ -1,16 +1,23 @@
 class Item 
-  attr_reader :name, :bids
+  attr_reader :name, :bids, :closed
 
   def initialize(name)
     @name = name
-    @bids = {}
+    @bids = {} 
+    @closed = false
   end
 
   def add_bid(attendee_obj, bid_amount)
-   @bids[attendee_obj] = bid_amount
+   if @closed == false 
+    @bids[attendee_obj] = bid_amount
+   end
   end
 
   def current_high_bid 
     @bids.values.max
+  end
+
+  def close_bidding 
+    @closed = true
   end
 end
