9d2712d6ddd5a3cb7dcf97d8d7f68aabd7527f4c
KaraJH <karas.jones@yahoo.com>
2023-01-08 12:13:47 -0700

    test: Potential revenue method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -60,5 +60,22 @@ RSpec.describe Auction do
     end
   end
 
+  describe '#potential_revenue' do 
+    it 'is the total possible sale price of the items(items highest bid)' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20) 
+      item1.add_bid(attendee1, 22) 
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15) 
+      
+      expect(auction.potential_revenue).to eq(87)
+    end
+  end
+
   
 end
